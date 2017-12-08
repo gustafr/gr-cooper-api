7cccb2ffa914262fbbf71ff098f31f0b34974541
Gustaf Rudholm <gustafrudholm@Gustafs-MacBook-Pro.local>
2017-12-08 09:02:15 +0100

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
+  has_many :performance_data, class_name: 'PerformanceData'
+
+end
